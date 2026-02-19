d82cd6e938ec650e15f1c39522fd980f458dc9f9
Villy Siu <villysiu@gmail.com>
2026-02-18 21:48:10 -0800

    changed name

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { register, login, updateUser } = require('../controllers/auth')
+const { register, login, updateCredential } = require('../controllers/auth')
 const authenticateUser = require('../middleware/authentication');
 
 router.post('/register', register)
